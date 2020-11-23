e2ac72befe84b0ed69068a91aec2b15a7315de8f
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-23 14:49:11 +0000

    multiplying 2 numbers

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+  number_one = int(input('please choose a number:'))
+  number_two = int(input('please choose a number:'))
+  
+  print(str(number_one) + ' + ' + str(number_two) + ' = ' + str(number_one * number_two))
 
+  
 if __name__ == '__main__':
     main()
